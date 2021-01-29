2798137a6a32d2341ddd58fd6b643940bad0fd32
(uλ) Hidden <36114332+LambdaHidden@users.noreply.github.com>
2021-01-29 17:13:51 -0300

    Add files via upload

---
+++ lua/autorun/client/ut2004umenu.lua
@@ -0,0 +1,16 @@
+local function UT2004_SettingsPanel(Panel)
+	Panel:AddControl("Label", {Text = "Server"})
+	Panel:AddControl("CheckBox", {Label = "Restrict Redeemer", Command = "ut2k4_restrictredeemer"})
+	Panel:AddControl("CheckBox", {Label = "Unlimited Ammo", Command = "ut2k4_unlimitedammo"})
+	Panel:AddControl("CheckBox", {Label = "Weapons Stay", Command = "ut2k4_weaponsstay"})
+	Panel:AddControl("Slider", {Label = "Shield Gun self-damage Force", Command = "ut2k4_shieldgun_impulse", Type = "Float", Min = 0, Max = 32})
+	Panel:AddControl("Label", {Text = "Client"})
+	Panel:AddControl("CheckBox", {Label = "Fire Lighting", Command = "ut2k4_lighting"})
+	Panel:AddControl("Slider", {Label = "Viewmodel Bob", Command = "ut2k4_bobscale", Type = "Float", Min = 0, Max = 2})
+end
+
+local function UT2004_PopulateToolMenu()
+	spawnmenu.AddToolMenuOption("Utilities", "Unreal Tournament 2004", "UT2004Settings", "Settings", "", "", UT2004_SettingsPanel)
+end
+
+hook.Add("PopulateToolMenu", "UT2004_PopulateToolMenu", UT2004_PopulateToolMenu)
