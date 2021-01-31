65a524fa6f9d1e652cb0d195d82cf5ed2ac29523
(uλ) Hidden <36114332+LambdaHidden@users.noreply.github.com>
2021-01-31 19:33:19 -0300

    Update shared.lua
    Replaced all ".Owner" with ":GetOwner()".

--- lua/weapons/weapon_ut2004_linkgun/shared.lua
+++ lua/weapons/weapon_ut2004_linkgun/shared.lua
@@ -25,7 +25,7 @@ function SWEP:SpecialInit()
 	--self:SetHoldType(self.HoldType)
 	util.PrecacheSound(self.Primary.Sound)
 	util.PrecacheSound(self.Secondary.Sound)
-	self.Owner:SetNWInt("LinkGunLinks", 0)
+	self:GetOwner():SetNWInt("LinkGunLinks", 0)
 	self.Link = nil
 end
 
@@ -40,14 +40,14 @@ function SWEP:PrimaryAttack()
 	self:UDSound()
 	
 	if SERVER then
-		local pos = self.Owner:GetShootPos()
-		local ang = self.Owner:GetAimVector():Angle()
+		local pos = self:GetOwner():GetShootPos()
+		local ang = self:GetOwner():GetAimVector():Angle()
 		pos = pos +ang:Right() *math.random(4,8) +ang:Up() *math.random(0,-6)
 		local ent = ents.Create("ut2004_link_proj")
 		ent:SetAngles(ang)
 		ent:SetPos(pos)
-		ent:SetOwner(self.Owner)
-		ent.Links = self.Owner:GetNWInt("LinkGunLinks")
+		ent:SetOwner(self:GetOwner())
+		ent.Links = self:GetOwner():GetNWInt("LinkGunLinks")
 		ent:Spawn()
 		ent:Activate()
 		local phys = ent:GetPhysicsObject()
@@ -62,11 +62,11 @@ function SWEP:SecondaryAttack()
 
 	if !self:GetSecAttack() then
 		if !self.LoopSound or (self.LoopSound and !self.LoopSound:IsPlaying()) then
-			self.LoopSound = CreateSound(self.Owner, self.Secondary.Sound)
+			self.LoopSound = CreateSound(self:GetOwner(), self.Secondary.Sound)
 			self.LoopSound:SetSoundLevel(100)
 			self.LoopSound:Play()
 			local fx = EffectData()
-			fx:SetEntity(self.Owner)
+			fx:SetEntity(self:GetOwner())
 			self.Effect = util.Effect("ut2004_link_beam", fx)
 		end
 		self:UDSound()
@@ -76,13 +76,13 @@ function SWEP:SecondaryAttack()
 	self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 	self:SetNextSecondaryFire(CurTime() + self.Secondary.Delay)
 	self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
+	self:GetOwner():SetAnimation(PLAYER_ATTACK1)
 	self:TakeAmmo()
 	self:DisableHolster(.08)
 end
 
 function SWEP:Beam()
-	if self.Owner:KeyDown(IN_ATTACK) then return end
+	if self:GetOwner():KeyDown(IN_ATTACK) then return end
 	if !self:CanPrimaryAttack() then return end
 	
 	if self.Link != nil then 
@@ -90,31 +90,31 @@ function SWEP:Beam()
 		return 
 	end
 	
-	if SERVER then self.Owner:LagCompensation(true) end
+	if SERVER then self:GetOwner():LagCompensation(true) end
 	local tr = util.TraceLine({
-		start = self.Owner:GetShootPos(),
-		endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * self.HitDist,
-		filter = self.Owner
+		start = self:GetOwner():GetShootPos(),
+		endpos = self:GetOwner():GetShootPos() + self:GetOwner():GetAimVector() * self.HitDist,
+		filter = self:GetOwner()
 	})
 		
 	if SERVER then
 		if tr.HitNonWorld then
-			if tr.Entity:IsPlayer() and tr.Entity:Team() == self.Owner:Team() then
+			if tr.Entity:IsPlayer() and tr.Entity:Team() == self:GetOwner():Team() then
 				self.Link = tr.Entity
-				--table.insert(self.Link.LinkGunLinks, self.Owner)
+				--table.insert(self.Link.LinkGunLinks, self:GetOwner())
 				self.Link:SetNWInt("LinkGunLinks", self.Link:GetNWInt("LinkGunLinks") + 1)
 			else
 				local dmginfo = DamageInfo()
-				dmginfo:SetDamage(self.Secondary.Damage * (1 + self.Owner:GetNWInt("LinkGunLinks") * 1.5) * engine.TickInterval())
+				dmginfo:SetDamage(self.Secondary.Damage * (1 + self:GetOwner():GetNWInt("LinkGunLinks") * 1.5) * engine.TickInterval())
 				dmginfo:SetDamageType(DMG_ENERGYBEAM)
-				dmginfo:SetAttacker(self.Owner)
+				dmginfo:SetAttacker(self:GetOwner())
 				dmginfo:SetInflictor(self)
 				dmginfo:SetDamagePosition(tr.HitPos)
-				--dmginfo:SetDamageForce(self.Owner:GetUp() * 3500 + self.Owner:GetForward() * 10000)
+				--dmginfo:SetDamageForce(self:GetOwner():GetUp() * 3500 + self:GetOwner():GetForward() * 10000)
 				tr.Entity:TakeDamageInfo(dmginfo)
 			end
 		end
-		self.Owner:LagCompensation(false)
+		self:GetOwner():LagCompensation(false)
 	end
 	--util.ParticleTracerEx( "ut2004_link_beam", tr.StartPos, tr.HitPos, false, self:EntIndex(), 1 )
 	
@@ -126,7 +126,7 @@ function SWEP:Beam()
 	
 	if !cvars.Bool("ut2k4_lighting") or !CLIENT then return end
 	local dynlight = DynamicLight(self:EntIndex())
-		dynlight.Pos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 40
+		dynlight.Pos = self:GetOwner():GetShootPos() + self:GetOwner():GetAimVector() * 40
 		dynlight.Size = 80
 		dynlight.Decay = 0
 		dynlight.R = 50
@@ -149,13 +149,13 @@ function SWEP:UpdateLinks(ply, adding)
 end
 */
 function SWEP:SpecialThink()
-	if self:GetSecAttack() and self.Owner:KeyReleased(IN_ATTACK2) then
+	if self:GetSecAttack() and self:GetOwner():KeyReleased(IN_ATTACK2) then
 		self:SetSecAttack(false)
 		if self.LoopSound then self.LoopSound:Stop() end
 		if self.Effect then self.Effect:Remove() end
 		
 		if self.Link != nil then
-			--table.RemoveByValue(self.Link.LinkGunLinks, self.Owner)
+			--table.RemoveByValue(self.Link.LinkGunLinks, self:GetOwner())
 			self.Link:SetNWInt("LinkGunLinks", self.Link:GetNWInt("LinkGunLinks") - 1)
 			self.Link = nil
 		end
@@ -171,7 +171,7 @@ end
 function SWEP:AttackStuff()	
 	self:MuzzleflashSprite()
 	self:TakeAmmo()
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
+	self:GetOwner():SetAnimation(PLAYER_ATTACK1)
 	--self:UDSound()
 end
 
